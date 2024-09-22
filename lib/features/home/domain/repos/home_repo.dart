ef1e836b3c3a52054e977a69f98cb32f08b12b2f
Osama Mohamed <osamarizk262@gmail.com>
2024-09-22 18:07:24 +0300

    add either class

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> featchFeaturedBooks();
-  Future<List<BookEntity>> featchNewstBooks();
+  Future<Either<Failure, List<BookEntity>>> featchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> featchNewstBooks();
 }
