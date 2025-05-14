32dd3206f79418e3638f344287f00e9d787e5de1
Osama Mohamed <osamarizk262@gmail.com>
2025-05-14 22:20:43 +0300

    add get it for service locator

---
+++ lib/core/utils/functions/setup_service_locator.dart
@@ -0,0 +1,23 @@
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:bookly/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final GetIt getIt = GetIt.instance;
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(
+    ApiService(
+      dio: Dio(),
+    ),
+  );
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(
+      homeRemoteDataSource: HomeRemoteDataSourceImpl(
+        getIt.get<ApiService>(),
+      ),
+      homeLocalDataSource: HomeLocalDataSourceImpl(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/consts.dart';
+import 'package:bookly/core/utils/functions/setup_service_locator.dart';
 import 'package:bookly/core/utils/router_app.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/use_case/featch_featured_book_use_case.dart';
+import 'package:bookly/features/home/domain/use_case/featch_newest_books_use_case.dart';
+import 'package:bookly/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/features/home/presentation/manger/newest_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 
@@ -11,6 +18,8 @@ void main() async {
   await Hive.openBox<BookEntity>(kFeaturedBooksBox);
   await Hive.openBox<BookEntity>(kNewestBooksBox);
   Hive.registerAdapter(BookEntityAdapter());
+  setupServiceLocator();
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const BooklyApp());
 }
 
@@ -19,13 +28,32 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: RouterApp.router,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            FeatchFeaturedBookUseCase(
+              getIt.get<HomeRepoImpl>(),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            FeatchNewestBooksUseCase(
+              getIt.get<HomeRepoImpl>(),
+            ),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: RouterApp.router,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
